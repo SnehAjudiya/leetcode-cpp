d76ca61a4b23f096272938e25f08a38754270f75
SnehAjudiya <snehajudiya33@gmail.com>
2025-07-04 17:20:34 +0530

    Add Two Sum solution

---
+++ two_sum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> m;
+    for (int i = 0; i < nums.size(); ++i) {
+        int need = target - nums[i];
+        if (m.count(need)) return {m[need], i};
+        m[nums[i]] = i;
+    }
+    return {};
+}
